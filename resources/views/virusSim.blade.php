1215005dc97ee9ef5a6a6280db511c768ab4ea42
JAParsons <johnparsons932@gmail.com>
2020-02-01 17:52:38 +0000

    slider triggers map change

--- resources/views/virusSim.blade.php
+++ resources/views/virusSim.blade.php
@@ -9,8 +9,53 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.min.js"></script>
     <script src={{asset('js/datamaps.js')}}></script>
 
+    <style>
+        .slidecontainer {
+            width: 30%; /* Width of the outside container */
+        }
+
+        /* The slider itself */
+        .slider {
+            -webkit-appearance: none;  /* Override default CSS styles */
+            appearance: none;
+            width: 100%; /* Full-width */
+            height: 25px; /* Specified height */
+            background: #d3d3d3; /* Grey background */
+            outline: none; /* Remove outline */
+            opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+            -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+            transition: opacity .2s;
+        }
+
+        /* Mouse-over effects */
+        .slider:hover {
+            opacity: 1; /* Fully shown on mouse-over */
+        }
+
+        /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+        .slider::-webkit-slider-thumb {
+            -webkit-appearance: none; /* Override default look */
+            appearance: none;
+            width: 25px; /* Set a specific slider handle width */
+            height: 25px; /* Slider handle height */
+            background: #4CAF50; /* Green background */
+            cursor: pointer; /* Cursor on hover */
+        }
+
+        .slider::-moz-range-thumb {
+            width: 25px; /* Set a specific slider handle width */
+            height: 25px; /* Slider handle height */
+            background: #4CAF50; /* Green background */
+            cursor: pointer; /* Cursor on hover */
+        }
+    </style>
+
+
 
     <div id="map" style="position: relative; width: 500px; height: 300px;"></div>
+    <div class="slidecontainer">
+        <input type="range" min="1" max="8" value="1" class="slider" id="slider">
+    </div>
 
 
 
@@ -34,16 +79,20 @@
 
         var colors = d3.scale.category10();
 
-        window.setInterval(function() {
+        function change() {
             basic_choropleth.updateChoropleth({
                 USA: colors(Math.random() * 10),
                 RUS: colors(Math.random() * 100),
-                AUS: { fillKey: 'authorHasTraveledTo' },
+                AUS: {fillKey: 'authorHasTraveledTo'},
                 BRA: colors(Math.random() * 50),
                 CAN: colors(Math.random() * 50),
                 ZAF: colors(Math.random() * 50),
                 IND: colors(Math.random() * 50),
             });
-        }, 2000);
+        }
+
+        document.getElementById("slider").addEventListener("change", function() {
+            change();
+        }, false);
     </script>
 @endsection
