02ee26d844ed292a78310f0baa5629660c12ae49
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2020-02-01 14:54:28 +0000

    Footer Blade intial Commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer id="footer" class="page-footer font-small blue bg-dark ">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-light text-center py-3">© Quackathon - 2020. All Rights Reserved
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
