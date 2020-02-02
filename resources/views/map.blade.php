f7f5b12cafa94320b2bdcaa1025dee4b16ee3fd8
JAParsons <johnparsons932@gmail.com>
2020-02-02 07:51:31 +0000

    finalised simulation

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,4 +1,10 @@
-<html>
+@extends('layouts.master')
+
+@section('title')
+    Virus Simulator
+@endsection
+
+@section('content')
 <head>
     <style>
         .slidecontainer {
@@ -55,10 +61,25 @@
 <button onclick="play()">Play</button>
 
 <script type="text/javascript">
+    var coronaData;
     var chart;
     var options;
 
     var data1, data2, data3;
+
+    function getCorona(){
+        $.ajax({
+            method: 'GET',
+            url: '{{route('corona')}}',
+            data: {_token: '{{Session::token()}}'}
+        })
+            .done(function (msg) {
+                console.log(msg.data);
+                coronaData = msg.data;
+                if(msg.success){
+                }
+            });
+    }
 
     function play(){
         document.getElementById('slider').value = 0;
@@ -66,42 +87,74 @@
         setTimeout(() => { document.getElementById('slider').value++; updateMap(data3); }, 4000);
     }
 
-    google.charts.load('current', {
-        'packages':['geochart'],
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-    });
-    google.charts.setOnLoadCallback(drawRegionsMap);
+    function predict(cases, density){
+        let percent = (density/310)*100;
+        //console.log(percent);
+        return parseInt(cases+(cases*percent)/100);
+    }
 
-    function drawRegionsMap() {
+    function drawChart() {
+        google.charts.load('current', {
+            'packages': ['geochart'],
+            // Note: you will need to get a mapsApiKey for your project.
+            // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+            'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+        });
+        google.charts.setOnLoadCallback(drawRegionsMap);
 
-        data1 = google.visualization.arrayToDataTable([
-            ['Country', 'Total Cases', 'Total Deaths'],
-            ['China',11901, 259], ['Japan',20 , 0], ['Thailand', 19, 0], ['Singapore', 18, 0], ['Hong Kong', 13, 0], ['Australia', 12, 0], ['South Korea', 12, 0], ['Taiwan', 10, 0], ['Malaysia', 8, 0], ['United States', 8, 0], ['Germany', 7, 0], ['Macao', 7, 0], ['Vietnam', 6, 0],
-            ['France', 6, 0],  ['United Arab Emirates', 4, 0],  ['Canada', 4, 0],  ['Italy', 2, 0],  ['Russia', 2, 0],  ['United Kingdom', 2, 0],  ['Sri Lanka', 1, 0],  ['Nepal', 1, 0],  ['Sweden', 1, 0],  ['Cambodia', 1, 0],  ['India', 1, 0],  ['Finland', 1, 0],  ['Philippines', 1, 0],  ['Spain', 1, 0]
-        ]);
+        function drawRegionsMap() {
 
-        data2 = google.visualization.arrayToDataTable([
-            ['Country',   'Corona Cases', 'Future Prediction'],
-            ['United Kingdom', 0, 0], ['Hong Kong', 8, 2], ['Russia', 12, 10*2], ['China', 17, 17*3]
-        ]);
+            // for(let i=0; i<coronaData.length; i++){
+            //     data1[i+1] = [coronaData[i].country, coronaData[i].cases_day1, 1];
+            // }
 
-        data3 = google.visualization.arrayToDataTable([
-            ['Country',   'Corona Cases', 'Future Prediction'],
-            ['United Kingdom', 20, 20], ['Hong Kong', 8, 2], ['Russia', 12, 10*2], ['China', 17, 17*3]
-        ]);
+            // data1 = google.visualization.arrayToDataTable([
+            //     ['Country', 'Total Cases', 'Total Deaths'],
+            //     ['China', 11901, 259], ['Japan', 20, 0], ['Thailand', 19, 0], ['Singapore', 18, 0], ['Hong Kong', 13, 0], ['Australia', 12, 0], ['South Korea', 12, 0], ['Taiwan', 10, 0], ['Malaysia', 8, 0], ['United States', 8, 0], ['Germany', 7, 0], ['Macao', 7, 0], ['Vietnam', 6, 0],
+            //     ['France', 6, 0], ['United Arab Emirates', 4, 0], ['Canada', 4, 0], ['Italy', 2, 0], ['Russia', 2, 0], ['United Kingdom', 2, 0], ['Sri Lanka', 1, 0], ['Nepal', 1, 0], ['Sweden', 1, 0], ['Cambodia', 1, 0], ['India', 1, 0], ['Finland', 1, 0], ['Philippines', 1, 0], ['Spain', 1, 0]
+            // ]);
+            //
+            // data2 = google.visualization.arrayToDataTable([
+            //     ['Country', 'Total Cases', 'Total Deaths'],
+            //     ['China', 23802, 518*2], ['Japan', 41*2, 0], ['Thailand', 27*2, 0], ['Singapore', 503*2, 0], ['Hong Kong', 738*2, 0], ['Australia', 12*2, 0], ['South Korea', 31*2, 0], ['Taiwan', 30*2, 0], ['Malaysia', 10*2, 0], ['United States', 8*2, 0], ['Germany', 12*2, 0], ['Macao', 7*2, 0], ['Vietnam', 6*2, 0],
+            //     ['France', 9, 0], ['United Arab Emirates', 5*2, 0], ['Canada', 4*2, 0], ['Italy', 3*2, 0], ['Russia', 2*2, 0], ['United Kingdom', 3*2, 0], ['Sri Lanka', 2*2, 0], ['Nepal', 1*2, 0], ['Sweden', 1*2, 0], ['Cambodia', 1*2, 0], ['India', 2*2, 0], ['Finland', 1*2, 0], ['Philippines', 2*2, 0], ['Spain', 1*2, 0]
+            // ]);
+            //
+            // data3 = google.visualization.arrayToDataTable([
+            //     ['Country', 'Total Cases', 'Total Deaths'],
+            //     ['China', 29391, 1036], ['Japan', 85*2, 2*2], ['Thailand', 38*2, 0], ['Singapore', 14064*2, 16*2], ['Hong Kong', 41949/2, 67*2], ['Australia', 12*2, 0], ['South Korea', 81*2, 0], ['Taiwan', 92*2, 0], ['Malaysia', 13*2, 0], ['United States', 8*2, 0], ['Germany', 21*2, 0], ['Macao', 488*2, 0], ['Vietnam', 12*2, 0],
+            //     ['France', 12*2, 0], ['United Arab Emirates', 6*2, 0], ['Canada', 4*2, 0], ['Italy', 4*2, 0], ['Russia', 2*2, 0], ['United Kingdom', 5*2, 0], ['Sri Lanka', 3*2, 0], ['Nepal', 1*2, 0], ['Sweden', 1*2, 0], ['Cambodia', 1*2, 0], ['India', 4*2, 0], ['Finland', 1*2, 0], ['Philippines', 4*2, 0], ['Spain', 1*2, 0]
+            // ]);
 
-        options = {
-            region: 'world', // world
-            colorAxis: {colors: ['#00853f', '#e31b23']},
-            backgroundColor: '#ffffff',
-            datalessRegionColor: '#DCDCDC',
-            defaultColor: '#f5f5f5',
-        };
+            data1 = google.visualization.arrayToDataTable([
+                ['Country', 'Total Cases', 'Total Deaths'],
+                ['Japan', 20, 0], ['Thailand', 19, 0], ['Singapore', 18, 0], ['Hong Kong', 13, 0], ['Australia', 12, 0], ['South Korea', 12, 0], ['Taiwan', 10, 0], ['Malaysia', 8, 0], ['United States', 8, 0], ['Germany', 7, 0], ['Macao', 7, 0], ['Vietnam', 6, 0],
+                ['France', 6, 0], ['United Arab Emirates', 4, 0], ['Canada', 4, 0], ['Italy', 2, 0], ['Russia', 2, 0], ['United Kingdom', 2, 0], ['Sri Lanka', 1, 0], ['Nepal', 1, 0], ['Sweden', 1, 0], ['Cambodia', 1, 0], ['India', 1, 0], ['Finland', 1, 0], ['Philippines', 1, 0], ['Spain', 1, 0]
+            ]);
 
-        chart = new google.visualization.GeoChart(document.getElementById('geochart-colors'));
-        chart.draw(data1, options);
+            data2 = google.visualization.arrayToDataTable([
+                ['Country', 'Total Cases', 'Total Deaths'],
+                ['Japan', 41*4, 0], ['Thailand', 27*4, 0], ['Singapore', 2*4, 0], ['Hong Kong', 2*4, 0], ['Australia', 12*4, 0], ['South Korea', 31*4, 0], ['Taiwan', 30*4, 0], ['Malaysia', 10*4, 0], ['United States', 8*4, 0], ['Germany', 12*4, 0], ['Macao', 7*4, 0], ['Vietnam', 6*4, 0],
+                ['France', 9, 0], ['United Arab Emirates', 5*4, 0], ['Canada', 4*4, 0], ['Italy', 3*4, 0], ['Russia', 2*4, 0], ['United Kingdom', 3*4, 0], ['Sri Lanka', 2*4, 0], ['Nepal', 1*4, 0], ['Sweden', 1*4, 0], ['Cambodia', 1*4, 0], ['India', 2*4, 0], ['Finland', 1*4, 0], ['Philippines', 2*4, 0], ['Spain', 1*4, 0]
+            ]);
+
+            data3 = google.visualization.arrayToDataTable([
+                ['Country', 'Total Cases', 'Total Deaths'],
+                ['Japan', 85*4, 2*4], ['Thailand', 38*4, 0], ['Singapore', 0, 16*4], ['Hong Kong', 1/2, 67*4], ['Australia', 12*4, 0], ['South Korea', 81*4, 0], ['Taiwan', 92*4, 0], ['Malaysia', 13*4, 0], ['United States', 8*4, 0], ['Germany', 21*4, 0], ['Macao', 1*4, 0], ['Vietnam', 12*4, 0],
+                ['France', 12*4, 0], ['United Arab Emirates', 6*4, 0], ['Canada', 4*4, 0], ['Italy', 4*4, 0], ['Russia', 2*4, 0], ['United Kingdom', 5*4, 0], ['Sri Lanka', 3*4, 0], ['Nepal', 1*4, 0], ['Sweden', 1*4, 0], ['Cambodia', 1*4, 0], ['India', 4*4, 0], ['Finland', 1*4, 0], ['Philippines', 4*4, 0], ['Spain', 1*4, 0]
+            ]);
+
+            options = {
+                region: 'world', // world
+                colorAxis: {colors: ['#8EFF22','#B3FF22','#C7FF22', '#DEFF22', '#F9FF22', '#FFF122', '#FFDD22', '#FFCC22', '#FFBF22', '#FF8D22', '#FF7922', '#FF5E22', '#FF4722', '#FF3D22', '#FF2922', '#FF2222']},
+                backgroundColor: '#ffffff',
+                datalessRegionColor: '#DCDCDC',
+                defaultColor: '#f5f5f5',
+            };
+
+            chart = new google.visualization.GeoChart(document.getElementById('geochart-colors'));
+            chart.draw(data1, options);
+        }
     }
 
     document.getElementById("slider").addEventListener("change", function() {
@@ -115,4 +168,4 @@
     }
 </script>
 </body>
-</html>
+@endsection
